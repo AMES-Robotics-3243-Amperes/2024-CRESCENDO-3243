e0c984e4eb44aea9809de05eaf945f770dac2123
Mothandman <117953018+Mothandman@users.noreply.github.com>
2024-02-05 16:47:00 -0700

    Clean up the meeerge

--- src/main/java/frc/robot/commands/shooter/CommandShooterStopInstant.java
+++ src/main/java/frc/robot/commands/shooter/CommandShooterStopInstant.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SubsystemShooter;
@@ -13,7 +13,7 @@ public class CommandShooterStopInstant extends Command {
   /** Creates a new CommandShooterStopInstant. */
   public CommandShooterStopInstant(SubsystemShooter subsystemShooter) {
     // Use addRequirements() here to declare subsystem dependencies.
-    
+
     m_SubsystemShooter = subsystemShooter;
   }
 
