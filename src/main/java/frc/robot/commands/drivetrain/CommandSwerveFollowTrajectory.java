bdc875842a2603e7d66308e5875b95778a6a9731
Crystal Stay <jessestay3@gmail.com>
2024-02-06 20:59:54 -0700

    Fixed a merge issue

--- src/main/java/frc/robot/commands/drivetrain/CommandSwerveFollowTrajectory.java
+++ src/main/java/frc/robot/commands/drivetrain/CommandSwerveFollowTrajectory.java
@@ -39,10 +39,6 @@ public class CommandSwerveFollowTrajectory extends Command {
     addRequirements(m_subsystem);
   }
 
-  public CommandSwerveFollowTrajectory(SubsystemSwerveDrivetrain subsystem, Trajectory trajectory) {
-    // Unimplemented TODO
-  }
-
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
